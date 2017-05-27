65fab78b381c629c50806a2c8571ad19e15111ad
wangyuchun <78609144@qq.com>
2017-05-27 14:44:40 +0800

    send user information by JWT

--- src/main/java/com/risetek/auth/server/oltu/servlet/UserResourceServlet.java
+++ src/main/java/com/risetek/auth/server/oltu/servlet/UserResourceServlet.java
@@ -2,6 +2,8 @@ package com.risetek.auth.server.oltu.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +15,8 @@ import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
 import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
+import org.apache.oltu.oauth2.jwt.JWT;
+import org.apache.oltu.oauth2.jwt.io.JWTWriter;
 import org.apache.oltu.oauth2.rs.request.OAuthAccessResourceRequest;
 import org.apache.oltu.oauth2.rs.response.OAuthRSResponse;
 
@@ -36,9 +40,44 @@ public class UserResourceServlet extends HttpServlet {
 			if(null == username)
 				throw OAuthProblemException.error("invalid client token");
 				
+			JWT.Builder builder = new JWT.Builder();
+			builder.setHeaderAlgorithm("RS256");
+			builder.setHeaderType("JWT");
+			builder.setHeaderContentType("JWT");
+			builder.setClaimsSetIssuer("accounts.risetek.com");
+			builder.setClaimsSetJwdId("UUID");
+
+			builder.setClaimsSetIssuedAt(new Date().getTime());
+			builder.setClaimsSetExpirationTime(new Date().getTime() + 1000 * 60 * 60 * 24 * 5);
+
+			builder.setClaimsSetSubject(username);
+
+			List<String> roles = userManagement.getRoles(username);
+			if(roles != null) {
+				StringBuffer sb = new StringBuffer();
+				for(String role:roles)
+					sb.append(role).append(":");
+				
+				builder.setClaimsSetCustomField("roles", sb.toString());
+			}
+
+			List<Integer> teams = userManagement.getTeams(username);
+			if(roles != null) {
+				StringBuffer sb = new StringBuffer();
+				for(Integer team:teams)
+					sb.append(team.toString()).append(":");
+				
+				builder.setClaimsSetCustomField("teams", sb.toString());
+			}
+			
+			builder.setSignature("risetek-yun74-id");
+			
+			JWTWriter writer = new JWTWriter();
+			String jwts = writer.write(builder.build());
 			response.setStatus(HttpServletResponse.SC_OK);
+
 			PrintWriter pw = response.getWriter();
-			pw.print(username);
+			pw.print(jwts);
 			pw.flush();
 			pw.close();
 		} catch (OAuthSystemException | OAuthProblemException e) {
